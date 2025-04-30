aa7b6d7e78bee0ec3f13291e37b693bac3b9d3c2
Alex <yuriev200@gmail.com>
2025-04-30 22:54:39 +0300

    abc

---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+export default function TestPage() {
+    const num = 4;
+
+    return (
+        <>
+            <main className="main" id="main">
+                hello
+            </main>
+        </>
+    );
+}
